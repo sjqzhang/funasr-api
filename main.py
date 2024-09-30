ab4b571eea3cfe0b482ff66edf9b3bafd697c703
小张 <s_jqzhang@163.com>
2024-09-30 15:11:00 +0800

    add web support

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from typing import List
 
 import torch
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import PlainTextResponse,HTMLResponse
 from funasr import AutoModel
+from fastapi.staticfiles import StaticFiles
 import json
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -40,30 +44,6 @@ async def save_upload_file(upload_file: UploadFile) -> str:
         return temp_file.name  # 返回临时文件路径
 
 
-def funasr_to_srt(funasr_result):
-    # 解析JSON字符串
-    # data = json.loads(funasr_result)
-    data=funasr_result
-    subtitles = data[0]['text'].split('. ')
-    timestamps = data[0]['timestamp']
-    
-    # 初始化SRT字符串
-    srt = ""
-    subtitle_index = 1
-    
-    # 遍历字幕和时间戳
-    for i in range(len(subtitles)):
-        if subtitles[i]:  # 确保字幕不为空
-            # 格式化时间戳
-            start_time = timestamps[i][0]
-            end_time = timestamps[i][1]
-            srt += f"{subtitle_index}\n"
-            srt += f"{format_timestamp(start_time)} --> {format_timestamp(end_time)}\n"
-            srt += f"{subtitles[i]}.\n\n"
-            subtitle_index += 1
-    
-    return srt
-
 def format_timestamp(milliseconds):
     # 将毫秒转换为SRT格式的时间戳
     seconds, milliseconds = divmod(milliseconds, 1000)
@@ -71,6 +51,129 @@ def format_timestamp(milliseconds):
     minutes, seconds = divmod(seconds, 60)
     return f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
 
+def funasr_to_srt(data):
+    text = data[0]['text']
+    timestamps = data[0]['timestamp']
+    punctuations = ".,。，!！"
+    sentences = []
+    start_idx = 0
+    for i, char in enumerate(text):
+        if char in punctuations:
+            sentences.append(text[start_idx:i])
+            start_idx = i+1
+    if start_idx < len(text):
+        sentences.append(text[start_idx:])
+
+    srt_data = []
+    all_len=0
+    end=0
+    for idx, sentence in enumerate(sentences):
+        try:
+            start=end
+            end+=len(sentence)
+            first_char_time = timestamps[start][0]
+            last_char_time=timestamps[end-1][1]
+        except IndexError:
+            continue
+
+        start_time = format_timestamp(first_char_time)
+        end_time = format_timestamp(last_char_time)
+
+        srt_data.append(f"{idx+1}\n{start_time} --> {end_time}\n{sentence.strip()}\n\n")
+
+    return ''.join(srt_data)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    # upload form template
+    return """
+    <!doctype html>
+    <title>Upload Audio File</title>
+    <h1>Upload Audio File</h1>
+    <script
+  src="https://code.jquery.com/jquery-3.7.1.js"
+  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+  crossorigin="anonymous"></script>
+    <form id="fileUploadForm" enctype="multipart/form-data">
+        <input type="file" name="file" id="file">
+        <input value="Upload" id="uploadBtn" name="uploadBtn" type="button" />
+        <span id="message"></span>
+        <input value="Download" id="downloadBtn" name="downloadBtn" type="button" />
+
+        <div style="display:block;width: 100%;height: 500px;">
+            <textarea name="text" id="text" cols="30" rows="10" style="height: 100%;width: 49.5%;float:left"></textarea>
+            <textarea name="srt" id="srt" cols="30" rows="10" style="height: 100%;width: 49.5%;float:right" ></textarea>
+        </div>
+    </form>
+
+    <script>
+
+    $(document).ready(function() {
+        function changeFileExtension(filePath) {
+            // 使用正则表达式分割路径和文件名
+            var parts = filePath.split('\\\\');
+            if (parts.length === 1) {
+                parts = filePath.split("/");
+            }
+            const fileNameWithExtension = parts[parts.length - 1]; // 获取最后一部分作为文件名
+            
+            // 分割文件名和扩展名
+            const fileNameParts = fileNameWithExtension.split('.');
+            
+            // 如果文件名没有扩展名，则直接添加 .srt
+            if (fileNameParts.length === 1) {
+                return `${fileNameWithExtension}.srt`;
+            }
+            
+            // 构建新的文件名，将扩展名替换为 .srt
+            const newFileName = `${fileNameParts.slice(0, -1).join('.')}.srt`;
+            
+            return newFileName;
+        }
+        function saveFile(text, filename) {
+            var blob = new Blob([text], { type: 'text/plain;charset=utf-8' });
+            var downloadLink = $('<a></a>')
+                .attr('href', URL.createObjectURL(blob))
+                .attr('download', filename)
+                .appendTo('body');
+            downloadLink[0].click();
+            downloadLink.remove();
+        }
+        $('#uploadBtn').click(function() {
+            
+            var formData = new FormData($('#fileUploadForm')[0]);
+
+            $('#message').html('Processing...');
+    
+            $.ajax({
+                url: '/asr', 
+                type: 'POST',
+                data: formData,
+                contentType: false,
+                processData: false,
+                success: function(response) {
+                    $('#message').html('Success');
+                    var result=response['result'][0];
+                    $('#text').html(result['text']);
+                    $('#srt').html(result['srt']);
+                },
+                error: function(xhr, status, error) {
+                $('#message').html('Processing Error');
+                    console.error('File upload failed:', status, error);
+                }
+            });
+        });
+
+        $('#downloadBtn').click(function() {
+            var filename=$('#file').val();
+            saveFile($('#srt').val(), changeFileExtension(filename));
+        });
+    });
+    </script>
+
+    """
+    
 
 @app.post("/asr")
 async def asr(file: List[UploadFile] = File(...)):
